bb7ac4803194b4e327f7f73c1343a6c55e26667f
waleedsebak-maker <waleedsebak@gmail.com>
2026-09-20 16:51:16 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,9 @@ android {
         applicationId = "com.example.gpsformautomator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+
+        versionCode = 2
+        versionName = "1.1"
     }
 }
 
